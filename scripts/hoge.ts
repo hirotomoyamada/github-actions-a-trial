192849b9226e350cc4eba5ff7bf61d3bb5184fda
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-11-30 10:43:56 +0900

    feat: added new hoge-16.json file

--- scripts/hoge.ts
+++ scripts/hoge.ts
@@ -11,7 +11,7 @@ const main = async () => {
   await mkdir(path.join(process.cwd(), '.hoge'), { recursive: true });
 
   await Promise.all(
-    Array(15)
+    Array(16)
       .fill(0)
       .map(async (_, index) => {
         await writeFile(path.join(process.cwd(), '.hoge', `hoge-${index + 1}.json`), json);
